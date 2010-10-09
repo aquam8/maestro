1292a14014f70c467be273c68752358b67bc7302
Matt Lohier <mlohier@gmail.com>
2010-10-10 09:25:22 +1100

    Trying to change ruby to use rvm

--- lib/maestro/operating_system/ubuntu.rb
+++ lib/maestro/operating_system/ubuntu.rb
@@ -5,6 +5,7 @@ module Maestro
       def initialize(etc_issue_str)
         super(etc_issue_str)
                 rvm_sh = "[[ -s '/usr/local/lib/rvm' ]] && . '/usr/local/lib/rvm'"
+                rvm_ruby_bin = "/usr/local/rvm/gems/ruby-1.9.2-p0/bin"
                 # http://alestic.com/2009/11/ec2-karmic-desktop#comment-237
                 @chef_install_script =
                   ["sh -c 'export DEBIAN_FRONTEND=noninteractive; sudo apt-get update -y'",
@@ -13,7 +14,7 @@ module Maestro
                    "bash < <( curl -L http://bit.ly/rvm-install-system-wide )",
                    "sudo echo '#{rvm_sh}' > /etc/profile.d/rvm.sh",
                    "rvm install ruby-1.9.2-p0 && rvm --default ruby-1.9.2-p0",
-                   "source /etc/profile && /usr/local/rvm/rubies/ruby-1.9.2-p0/bin/gem install ohai chef --no-rdoc --no-ri",
+                   "source /etc/profile && #{rvm_ruby_bin}/gem install ohai chef --no-rdoc --no-ri"
         #           "sudo mkdir -p /usr/local/src",
         #           "sudo wget -P /usr/local/src http://rubyforge.org/frs/download.php/69365/rubygems-1.3.6.tgz",
         #           "sudo tar zxf /usr/local/src/rubygems-1.3.6.tgz -C /usr/local/src",
@@ -23,7 +24,7 @@ module Maestro
         #           "sudo gem sources -a http://gems.opscode.com",
         #           "sudo gem install rake --no-rdoc --no-ri",
         #           "sudo gem install chef --no-rdoc --no-ri --version '= 0.9.8'",
-                   "sudo ln -nfs /usr/local/rvm/gems/ruby-1.9.2-p0/gems/chef-0.9.8/bin/chef-solo /usr/bin/chef-solo"
+                  # "sudo ln -sfv #{rvm_ruby_bin}/chef-solo /usr/bin/chef-solo"
                 #   "sudo ln -sfv $(gem environment gemdir)/gems/chef-0.9.8/bin/chef-solo /usr/bin/chef-solo"
                   ]
         # @chef_install_script =
